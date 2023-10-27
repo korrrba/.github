302648cea772b601473b4e07cb27882a6141d8a5
korrrba <88761781+gitcoindev@users.noreply.github.com>
2023-10-27 13:05:10 +0200

    Update print-deployments-logs.js

--- utils/print-deployments-logs.js
+++ utils/print-deployments-logs.js
@@ -18,7 +18,8 @@ module.exports = async ({ github, context, fs, customDomain }) => {
     }
     const slicedSha = commitSha.slice(0, -33);
 
-    defaultBody = `\<div align="right"\>\<p\>\<a href="${uniqueDeployUrl}"\>\<code\>${slicedSha}\</code\>\</a\>\</p\>\</div\>`;
+    defaultBody = `${slicedSha}`;
+    // defaultBody = `\<div align="right"\>\<p\>\<a href="${uniqueDeployUrl}"\>\<code\>${slicedSha}\</code\>\</a\>\</p\>\</div\>`;
   }
 
   const verifyInput = (data) => {
@@ -137,7 +138,7 @@ module.exports = async ({ github, context, fs, customDomain }) => {
 
     if (commentsArray.length > 0) {
       commentsArray.forEach((elem) => {
-        if (elem.user.type === "Bot" && elem.user.login === "ubiquibot[bot]") {
+        if (elem.user.type === "Bot" && elem.user.login === "korrrba-bot[bot]") {
           botCommentsArray.push(elem);
         }
       });
